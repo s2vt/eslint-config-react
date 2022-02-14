dbb14300ff3b58e5bb248cee6b525ebb3c9ff106
changhyun.kim <changhyun.kim@ssvt.co.kr>
2022-02-14 14:42:42 +0900

    Add prettier settings

---
+++ prettier.js
@@ -0,0 +1,9 @@
+const prettierSettings = require('./.prettierrc.json');
+
+module.exports = {
+  plugins: ['prettier'],
+  extends: ['prettier'],
+  rules: {
+    'prettier/prettier': ['error', prettierSettings],
+  },
+};
